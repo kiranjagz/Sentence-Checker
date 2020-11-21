a5bae3b8ac009fba028930f8b4c6fabfe7943f6d
Kiran Jagathlal <kiranjagz@gmail.com>
2020-11-21 12:56:42 +0200

    Added selection options on the console program.

--- Sentence.Checker.Core/Program.cs
+++ Sentence.Checker.Core/Program.cs
@@ -10,20 +10,48 @@ namespace Sentence.Checker.Core
     {
         static void Main(string[] args)
         {
+            string output = string.Empty;
+
             var customSentenceFormatter = new CustomSentenceFormatter();
 
             var sentenceService = new SentenceService(customSentenceFormatter);
 
-            Console.WriteLine("Please enter a sentence to analyze!");
+            Console.WriteLine("Please enter text to be analyzed!");
             var sentence = Console.ReadLine();
 
-            Console.WriteLine("Please select an option!");
+            Console.WriteLine("Enter which operations to do on the supplied text!");
+            Console.WriteLine("'1', for a duplicate character check.");
+            Console.WriteLine("'2', to count the number of vowels.");
+            Console.WriteLine("'3', to check if there are more vowels or non vowels.");
+            Console.WriteLine("Or any combination of '1','2','3' to perform multiple checks.");
             var option = Console.ReadLine();
 
+            //Wrap this in a selection service, and create some tests for them
             switch (option)
             {
-
+                case "1":
+                    {
+                        output = sentenceService.CheckForDuplicates(sentence).Output;
+                        break;
+                    };
+                case "2":
+                    {
+                        output = sentenceService.CountNumberOfVowels(sentence).Output;
+                        break; ;
+                    };
+                case "3":
+                    {
+                        output = sentenceService.CompareVowelsToNonVowels(sentence).Output;
+                        break;
+                    };
+                default:
+                    {
+                        output = "Opps sorry, not selection made :|";
+                        break;
+                    }
             };
+
+            Console.WriteLine(output);
 
             Console.Read();
         }
